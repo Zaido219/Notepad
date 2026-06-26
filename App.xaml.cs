4d7bb2abcf4a92c1c1aef11c8389c8925b8762e0
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-06-26 07:41:41 -0700

    feat:WIP Data binding and initial core functionality of the notepad,Temporarily not using command layer

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,28 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using NotepadApp.ViewModels;
+using NotepadApp.Views;
 
-namespace NotepadApp;
+namespace NotepadApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // create the layers
+            MainWindow window = new MainWindow();
+            IMainViewModel viewModel = new MainViewModel();
+            // hand the view model to the view
+            window.DataContext = viewModel;
+            // display the ui
+            window.Show();
+        }
+    }
+
+}
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
-{
-}
-
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,24 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using NotepadApp.ViewModels;
 
-namespace NotepadApp.Views;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace NotepadApp.Views
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent(); // This will work now!
+        }
+
+        private void NewMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is IMainViewModel viewModel)
+            {
+                viewModel.NewDocument();
+            }
+        }
     }
-}
+} // Make sure this closing brace is at the very end of your file
